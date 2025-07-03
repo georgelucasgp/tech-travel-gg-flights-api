4aae2a750da115c2b5c14e5fc72821ceb813fe92
George Lucas <djgeorgelucasgp@gmail.com>
2025-07-03 14:17:13 -0300

    feat(modules): add Availability and Users modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { PrismaModule } from './shared/infrastructure/database/prisma.module';
 import { FlightsModule } from './modules/flights/flights.module';
 import { ItinerariesModule } from './modules/itineraries/itineraries.module';
 import { BookingsModule } from './modules/bookings/bookings.module';
+import { AvailabilityModule } from './modules/availability/availability.module';
 import { AirlinesModule } from './modules/airlines/airlines.module';
 import { AirportsModule } from './modules/airports/airports.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -14,8 +16,10 @@ import { AirportsModule } from './modules/airports/airports.module';
     FlightsModule,
     ItinerariesModule,
     BookingsModule,
+    AvailabilityModule,
     AirlinesModule,
     AirportsModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
