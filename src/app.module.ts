51a62fbda455b97f97e83eb86cfd74dfa4050ce6
George Lucas <djgeorgelucasgp@gmail.com>
2025-07-03 07:26:03 -0300

    feat(modules): add Itineraries, Bookings, Airlines, and Airports modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './shared/infrastructure/database/prisma.module';
 import { FlightsModule } from './modules/flights/flights.module';
+import { ItinerariesModule } from './modules/itineraries/itineraries.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { AirlinesModule } from './modules/airlines/airlines.module';
+import { AirportsModule } from './modules/airports/airports.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     FlightsModule,
+    ItinerariesModule,
+    BookingsModule,
+    AirlinesModule,
+    AirportsModule,
   ],
   controllers: [],
   providers: [],
